1832b6bada9c3374ff4e522ac075baddf382828c
unknown <sjjoseph106@gmail.com>
2021-11-18 12:59:39 +0900

    Setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import { useState } from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Home from "./screens/Home";
-import Login from "./screens/Login";
-import NotFound from "./screens/NotFound";
+import Home from "./Screens/Home";
+import Login from "./Screens/Login";
+import Signup from "./Screens/Signup";
+import routes from "./routes";
  
 function App() {
-const [isLoggedIn,setIsLoggedIn] = useState(false);
-//Home Component와 Login Component에 있는 방식은 함수나 변수를 Component에 넘겨주는 방법
-//name(아무거나)={할당할 변수 or 함수}
-//여기서 만든 name을 각 Component에 작성하기
-//기능은 isLoggedIn이 true(로그인함) / false(로그인 안함)을 확인
 
   return (
     <div>
       <Router>
         <Switch>
-          <Route path="/" exact>
-            {isLoggedIn ? <Home setIsLoggedIn={setIsLoggedIn}/> : <Login setIsLoggedIn={setIsLoggedIn} />}
+          <Route path={routes.home} exact>
+            <Home />
           </Route>
-          <Route>
-            <NotFound/>
+          <Route path={routes.logIn} exact>
+            <Login />
+          </Route>
+          <Route path={routes.signUp} exact>
+            <Signup />
           </Route>
         </Switch>
       </Router>
